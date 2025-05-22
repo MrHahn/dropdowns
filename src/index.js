b56454a02dc41a4630ec30f3190f2ae1b052d5a0
Christopher Hahn <chahn970@gmail.com>
2025-05-21 18:09:20 -0600

    add close toggle functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+let dropdowns = document.querySelectorAll('.has-child-menu');
+
+dropdowns.forEach((item) => {
+    let submenu = item.querySelector('.submenu');
+    const nav = document.querySelector('nav');
+    item.addEventListener('click', (event) => {
+        dropdowns.forEach((dropdown) => {
+            dropdown.querySelector('.submenu').classList.remove('show');
+        })
+        submenu.classList.add('show');
+    })
+    document.addEventListener('click', (event) => {
+        const clickedElement = event.target;
+        if(nav && !nav.contains(clickedElement) && clickedElement !== nav){
+            submenu.classList.remove('show');
+        }
+    })
+})
