a9d0926a6593c6eed97a93b8b0f1b226ad27e72e
Christopher Hahn <chahn970@gmail.com>
2025-05-23 17:37:57 -0600

    run prettier

--- src/dropdown.js
+++ src/dropdown.js
@@ -1,5 +1,3 @@
-export default class Dropdown{
-    constructor(){
-
-    }
+export default class Dropdown {
+  constructor() {}
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import "./styles.css";
 
-let dropdownParents = document.querySelectorAll('.has-child-menu');
+let dropdownParents = document.querySelectorAll(".has-child-menu");
 
 dropdownParents.forEach((item) => {
-    let submenu = item.querySelector('.submenu');
-    const nav = document.querySelector('nav');
-    item.addEventListener('click', (event) => {
-        event.stopPropagation();
-        if(!event.target.classList.contains('child-link')){
-            dropdownParents.forEach((dropdown) => {
-                dropdown.querySelector('.submenu').classList.remove('show');
-            })
-        }
-        submenu.classList.add('show');
-    })
-    document.addEventListener('click', (event) => {
-        const clickedElement = event.target;
-        if(nav && !nav.contains(clickedElement) && clickedElement !== nav){
-            submenu.classList.remove('show');
-        }
-    })
-})
+  let submenu = item.querySelector(".submenu");
+  const nav = document.querySelector("nav");
+  item.addEventListener("click", (event) => {
+    event.stopPropagation();
+    if (!event.target.classList.contains("child-link")) {
+      dropdownParents.forEach((dropdown) => {
+        dropdown.querySelector(".submenu").classList.remove("show");
+      });
+    }
+    submenu.classList.add("show");
+  });
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    if (nav && !nav.contains(clickedElement) && clickedElement !== nav) {
+      submenu.classList.remove("show");
+    }
+  });
+});
